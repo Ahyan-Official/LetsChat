0f8e201d3a09ee01cc05ea73df722217176da199
Ahyan <azronahyan@gmail.com>
2020-08-25 00:40:33 +0500

    Datting Ik

---
+++ app/src/main/java/com/dating/ik/app2x3/Chat2Activity.java
@@ -0,0 +1,13 @@
+package com.dating.ik.app2x3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Chat2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat2);
+    }
+}
---
+++ app/src/main/java/com/dating/ik/app2x3/Fragments/Friends.java
@@ -0,0 +1,23 @@
+package com.dating.ik.app2x3.Fragments;
+
+
+public class Friends {
+
+    public String date;
+
+    public Friends(){
+
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public Friends(String date) {
+        this.date = date;
+    }
+}
